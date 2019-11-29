5801509f34224e3eca1de48d44a898ed0a7d6bc1
96bp19 <lanthoo170@gmail.com>
2019-11-29 14:50:11 +0545

    Fall and Crash game over added

---
+++ BumpyRoad/Assets/scripts/GameOverChecker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverChecker : MonoBehaviour
+{
+
+    public static bool gameOver;
+    public float distanceBelowGroundForDeathTrigger = 20f;
+
+    private void FixedUpdate()
+    {
+        if (gameOver)
+        {
+            return;
+        }
+        if (transform.position.y < -distanceBelowGroundForDeathTrigger)
+        {
+            gameOver = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ground"))
+        {
+            Debug.Log("Player died");
+            gameOver = true;
+        }
+    }
+}
