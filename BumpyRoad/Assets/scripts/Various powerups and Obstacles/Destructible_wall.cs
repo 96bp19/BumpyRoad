887053963aecd58386361a5822d57d8a93d5a8a1
manjil thapa <ijpt911@gmail.com>
2019-12-03 13:52:59 +0545

    breakable wall

---
+++ BumpyRoad/Assets/scripts/Various powerups and Obstacles/Destructible_wall.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructible_wall : MonoBehaviour
+{
+	public GameObject pieces_mesh;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Instantiate(pieces_mesh,transform.position,Quaternion.Euler(0,90,0)); 
+            Destroy(gameObject);
+        }
+    }
+
+    
+
+}
